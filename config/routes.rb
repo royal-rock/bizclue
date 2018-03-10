d39ed606796d427485f3b6825395a3beb32d8b3e
royal-rock <jjjjjjjjjjjjun@gmail.com>
2018-03-10 17:45:01 +0900

    はじめてのコミット

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  get 'home/index'
+
+  get 'home/authentication'
+
+  devise_for :users
+  get '/' => "home#top"
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
